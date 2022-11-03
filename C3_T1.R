5cdce8fb1cd9c3e4560122e403ca8d521250037c
RobertoLe <69279646+RobertoLe@users.noreply.github.com>
2022-11-03 13:33:00 -0600

    Add files via upload

---
+++ C3_T1.R
@@ -0,0 +1,10 @@
+# Descripción: It is the tutorial of how to use R with some regression models.
+#
+# Autor: Roberto Alejandro Leyva Márquez
+#
+# Fecha: 2022-07-28
+##########################D#####################################################
+
+
+iris <- read.csv("~/Downloads/R Tutorial Data Sets/iris.csv")
+cars <- read.csv("~/Downloads/R Tutorial Data Sets/cars.csv")
